170cc1019f22cdc21e257a46bc8446f7890b40b8
Brian Louis <brianlouis.salinas116@gmail.com>
2026-01-02 01:00:52 -0700

    Java setted up perfectly

---
+++ Syntax Practice/ReadKeyboard.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import java.util.*;
+
+
+
+public class ReadKeyboard {
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
+}
+
---
+++ variable.java
@@ -0,0 +1,7 @@
+public class variable {
+    public static void main(String[] args) {
+        int age = 18;
+        System.out.print(age);
+    }
+    
+}
