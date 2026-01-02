e7aee1ae9a8dd7a1a1199ea45f7c5220d5db115f
Brian Louis <brianlouis.salinas116@gmail.com>
2026-01-02 02:02:06 -0700

    If Statement

--- variable.java
+++ variable.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
+
 public class variable {
     public static void main(String[] args) {
         int age = 18;
-        System.out.print(age);
+        System.out.print(age + "\n");
+
+        Scanner ageAsk = new Scanner(System.in);
+
+        System.out.println("How old are you?: ");
+        age = ageAsk.nextInt();
+
+        System.out.println("You are: " + age);
+        if (age < 18) {
+            System.out.println("You cannot Drink! ");
+        }
+        else {
+            System.out.println("You can Drink!");
+        }
+
+
+
+
+        ageAsk.close();
+
+
     }
     
 }
