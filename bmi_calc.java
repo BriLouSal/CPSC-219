fdf91593f3f245e467ba23e367510c023fc23500
Brian Louis <brianlouis.salinas116@gmail.com>
2026-01-02 17:04:23 -0700

    BMI java solved

---
+++ bmi_calc.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class bmi_calc {
+    public static void main(String[] args) {
+        Scanner ask_weight = new Scanner(System.in);
+        System.out.print("What is your weight in kg: ");
+        Double weight = ask_weight.nextDouble();
+        
+        Scanner ask_heightScanner = new Scanner(System.in);
+        System.out.print("What is your height in meters: ");
+        Double height = ask_heightScanner.nextDouble();
+
+
+        Double calculate_bmi = (weight / (Math.pow(height, 2)));
+
+
+        if (calculate_bmi < 18.5){
+            System.out.print("You are Underweight");
+        }
+        else if (calculate_bmi >= 18.5 && calculate_bmi > 25){
+            System.out.print("You are Normal Weight");
+        }
+        else if (calculate_bmi >= 25 && calculate_bmi > 30){
+            System.out.print("You are  Overweight");
+        }
+        else{
+            System.out.print("You are Obese");
+        }
+
+
+
+
+
+
+        
+        ask_weight.close();
+
+    }
+    
+}
